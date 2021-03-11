438853ba0882433c4b142b1dd006e9b655332a77
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2021-03-11 14:47:01 +0300

    added log level

--- src/main/java/systems/microservice/loghub/sdk/LogLevel.java
+++ src/main/java/systems/microservice/loghub/sdk/LogLevel.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.sdk;
 
 /**
@@ -5,4 +22,15 @@ package systems.microservice.loghub.sdk;
  * @since 1.0
  */
 public class LogLevel {
+    public static final int OFF = 0;
+    public static final int FATAL = 100;
+    public static final int ERROR = 200;
+    public static final int WARN = 300;
+    public static final int INFO = 400;
+    public static final int DEBUG = 500;
+    public static final int TRACE = 600;
+    public static final int ALL = Integer.MAX_VALUE;
+
+    private LogLevel() {
+    }
 }

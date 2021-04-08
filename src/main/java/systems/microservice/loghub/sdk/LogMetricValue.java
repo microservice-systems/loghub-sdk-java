a00be7aeab86fbe4c22f7d0bc9c4f6a2f313bc00
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2021-04-09 02:55:49 +0300

    added buffering for log metric value

--- src/main/java/systems/microservice/loghub/sdk/LogMetricValue.java
+++ src/main/java/systems/microservice/loghub/sdk/LogMetricValue.java
@@ -17,34 +17,45 @@
 
 package systems.microservice.loghub.sdk;
 
+import systems.microservice.loghub.sdk.buffer.BufferWriter;
+import systems.microservice.loghub.sdk.buffer.Bufferable;
+
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-final class LogMetricValue {
+final class LogMetricValue implements Bufferable {
+    private static final double[] POINTS = {1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001,
+                                            0.000000001, 0.0000000001, 0.00000000001, 0.000000000001, 0.0000000000001,
+                                            0.00000000000001};
+
     public final String name;
+    public final AtomicLong time;
     public final AtomicLong count;
     public final AtomicLong sum;
     public final AtomicLong min;
     public final AtomicLong max;
-    public final byte point;
+    public final int point;
     public final String unit;
 
     public LogMetricValue(String name, long value, int point, String unit) {
         this.name = name;
+        this.time = new AtomicLong(System.currentTimeMillis());
         this.count = new AtomicLong(1L);
         this.sum = new AtomicLong(value);
         this.min = new AtomicLong(value);
         this.max = new AtomicLong(value);
-        this.point = (byte) point;
+        this.point = point;
         this.unit = unit;
     }
 
     public boolean add(long begin, long end, long value) {
         long t = System.currentTimeMillis();
         if ((t >= begin) && (t < end)) {
+            time.set(t);
             count.incrementAndGet();
             sum.addAndGet(value);
             for (long m = min.get(); value < m; m = min.get()) {
@@ -57,5 +68,20 @@ final class LogMetricValue {
         } else {
             return false;
         }
+    }
+
+    @Override
+    public int write(byte[] buffer, int index, Map<String, Object> context) {
+        byte v = 1;
+        double p = POINTS[point];
+        index = BufferWriter.writeByte(buffer, index, v);
+        index = BufferWriter.writeString(buffer, index, name);
+        index = BufferWriter.writeLong(buffer, index, count.get());
+        index = BufferWriter.writeDouble(buffer, index, ((double) sum.get()) * p);
+        index = BufferWriter.writeDouble(buffer, index, ((double) min.get()) * p);
+        index = BufferWriter.writeDouble(buffer, index, ((double) max.get()) * p);
+        index = BufferWriter.writeByte(buffer, index, (byte) point);
+        index = BufferWriter.writeString(buffer, index, unit);
+        return index;
     }
 }

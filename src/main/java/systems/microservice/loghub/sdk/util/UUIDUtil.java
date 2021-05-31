1a833bb018d2c89c1c28fecdb68d49cbc2b5d659
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-05-31 23:45:26 +0300

    added uuid util (1)

--- src/main/java/systems/microservice/loghub/sdk/util/UUIDUtil.java
+++ src/main/java/systems/microservice/loghub/sdk/util/UUIDUtil.java
@@ -1,8 +1,37 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.sdk.util;
+
+import java.util.UUID;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class UUIDUtil {
+public final class UUIDUtil {
+    private UUIDUtil() {
+    }
+
+    public static UUID randomUUID() {
+        return new UUID(System.currentTimeMillis(), ThreadInfo.getInstance().random.nextLong());
+    }
+
+    public static UUID randomUUID(long time) {
+        return new UUID(time, ThreadInfo.getInstance().random.nextLong());
+    }
 }

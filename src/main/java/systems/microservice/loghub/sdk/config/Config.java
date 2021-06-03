5a3785cac067b585689acff9c62e95fe2732dcab
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-06-04 02:44:52 +0300

    added config get property static methods

--- src/main/java/systems/microservice/loghub/sdk/config/Config.java
+++ src/main/java/systems/microservice/loghub/sdk/config/Config.java
@@ -19,7 +19,11 @@ package systems.microservice.loghub.sdk.config;
 
 import systems.microservice.loghub.sdk.LogHub;
 import systems.microservice.loghub.sdk.buffer.Bufferable;
+import systems.microservice.loghub.sdk.config.extractors.ValueOfExtractor;
+import systems.microservice.loghub.sdk.util.Argument;
+import systems.microservice.loghub.sdk.util.MapUtil;
 import systems.microservice.loghub.sdk.util.PropertiesUtil;
+import systems.microservice.loghub.sdk.util.Range;
 import systems.microservice.loghub.sdk.util.ResourceUtil;
 
 import java.io.Serializable;
@@ -29,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Dmitry Kotlyarov
@@ -36,6 +41,7 @@ import java.util.UUID;
  */
 public final class Config implements Bufferable, Serializable {
     private static final long serialVersionUID = 1L;
+    private static final ConcurrentHashMap<String, Property> currentProperties = new ConcurrentHashMap<>(4096, 0.75f, 64);
 
     public final UUID uuid;
     public final Map<String, Property> properties;
@@ -46,6 +52,8 @@ public final class Config implements Bufferable, Serializable {
     public final long time;
 
     public Config(Iterable<Property> properties, String comment, URL url, String user, String commit) {
+        Argument.notNull("properties", properties);
+
         this.uuid = UUID.randomUUID();
         this.properties = createProperties(properties);
         this.comment = comment;
@@ -66,5 +74,49 @@ public final class Config implements Bufferable, Serializable {
             ps.put(p.key, p);
         }
         return Collections.unmodifiableMap(ps);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz) {
+        return getProperty(key, clazz, null, null, false, null);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz, T defaultValue) {
+        return getProperty(key, clazz, defaultValue, null, false, null);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz, T defaultValue, String unit) {
+        return getProperty(key, clazz, defaultValue, unit, false, null);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz, T defaultValue, String unit, boolean nullable) {
+        return getProperty(key, clazz, defaultValue, unit, nullable, null);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz, T defaultValue, String unit, boolean nullable, T[] possibleValues) {
+        Property p = currentProperties.get(key);
+        if (p == null) {
+            p = MapUtil.putIfAbsent(currentProperties, key, new Property(null, null, null, null, null, null, null, null, null, null, null, null));
+        }
+        return p.value.get(clazz);
+    }
+
+    public static <I, O> O getProperty(String key, Class<I> clazz, I defaultValue, String unit, boolean nullable, I[] possibleValues, Class<O> outputClass) {
+        return getProperty(key, clazz, defaultValue, unit, nullable, possibleValues, outputClass, ValueOfExtractor.getInstance());
+    }
+
+    public static <I, O> O getProperty(String key, Class<I> clazz, I defaultValue, String unit, boolean nullable, I[] possibleValues, Class<O> outputClass, Extractor<I, O> extractor) {
+        return null;
+    }
+
+    public static <T extends Comparable<T>> T getProperty(String key, Class<T> clazz, T defaultValue, String unit, boolean nullable, Range<T> rangeValues) {
+        return null;
+    }
+
+    public static <I extends Comparable<I>, O> O getProperty(String key, Class<I> clazz, I defaultValue, String unit, boolean nullable, Range<I> rangeValues, Class<O> outputClass) {
+        return getProperty(key, clazz, defaultValue, unit, nullable, rangeValues, outputClass, ValueOfExtractor.getInstance());
+    }
+
+    public static <I extends Comparable<I>, O> O getProperty(String key, Class<I> clazz, I defaultValue, String unit, boolean nullable, Range<I> rangeValues, Class<O> outputClass, Extractor<I, O> extractor) {
+        return null;
     }
 }

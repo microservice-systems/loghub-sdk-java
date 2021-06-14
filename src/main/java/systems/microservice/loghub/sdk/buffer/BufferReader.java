d2f39686d3471fbe26831e985c364cecb8152a08
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-06-14 12:20:21 +0300

    added read date array method implementation to buffer reader

--- src/main/java/systems/microservice/loghub/sdk/buffer/BufferReader.java
+++ src/main/java/systems/microservice/loghub/sdk/buffer/BufferReader.java
@@ -656,6 +656,29 @@ public final class BufferReader {
     }
 
     public Date[] readDateArray() {
+        int idx = index;
+        byte ver = readVersion();
+        if (ver == 1) {
+            int l = readLength(1);
+            Date[] v = new Date[Math.min(l, ARRAY_READ_LENGTH_MAX)];
+            for (int i = 0; i < l; ++i) {
+                if (i >= v.length) {
+                    Date[] nv = new Date[v.length * 2];
+                    System.arraycopy(v, 0, nv, 0, v.length);
+                    v = nv;
+                }
+                v[i] = readDate();
+            }
+            if (l == v.length) {
+                return v;
+            } else {
+                Date[] nv = new Date[l];
+                System.arraycopy(v, 0, nv, 0, l);
+                return nv;
+            }
+        } else {
+            throw new BufferException(String.format("Buffer of size %d has illegal format at index %d: illegal version value %d", buffer.length, idx, ver));
+        }
     }
 
     public Color[] readColorArray() {

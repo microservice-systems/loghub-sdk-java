e7112da51bc2e4644532abc683bd256489492d67
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-07-23 04:15:49 +0300

    added property class

--- src/main/java/systems/microservice/loghub/sdk/Property.java
+++ src/main/java/systems/microservice/loghub/sdk/Property.java
@@ -1,8 +1,49 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.sdk;
+
+import systems.microservice.loghub.sdk.buffer.BufferObjectType;
+import systems.microservice.loghub.sdk.buffer.Bufferable;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class Property {
+public class Property<T> implements Bufferable, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public Property() {
+    }
+
+    public T get() {
+        return null;
+    }
+
+    @Override
+    public int write(byte[] buffer, int index, Map<String, Object> context) {
+        return 0;
+    }
+
+    static {
+        BufferObjectType.registerBufferableClass(UUID.fromString("35f98446-317a-49a9-ba79-8d931d22113a"), Property.class);
+    }
 }

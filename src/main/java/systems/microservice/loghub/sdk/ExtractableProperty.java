3b84adea598c7f654a258dc1746f05851c0fa439
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-07-27 23:31:18 +0300

    added extractable property class implementation

--- src/main/java/systems/microservice/loghub/sdk/ExtractableProperty.java
+++ src/main/java/systems/microservice/loghub/sdk/ExtractableProperty.java
@@ -17,9 +17,106 @@
 
 package systems.microservice.loghub.sdk;
 
+import systems.microservice.loghub.sdk.buffer.BufferObjectType;
+import systems.microservice.loghub.sdk.buffer.BufferWriter;
+import systems.microservice.loghub.sdk.buffer.Bufferable;
+import systems.microservice.loghub.sdk.config.Config;
+import systems.microservice.loghub.sdk.config.ConfigExtractor;
+import systems.microservice.loghub.sdk.config.extractor.ValueOfConfigExtractor;
+import systems.microservice.loghub.sdk.util.Argument;
+import systems.microservice.loghub.sdk.util.Range;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class ExtractableProperty {
+public class ExtractableProperty<I, O> implements Bufferable, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected final String key;
+    protected final Class<I> clazz;
+    protected final boolean nullable;
+    protected final I defaultValue;
+    protected final String unit;
+    protected final I[] possibleValues;
+    protected final Class<O> outputClass;
+    protected final ConfigExtractor<I, O> extractor;
+
+    public ExtractableProperty(String key, Class<I> clazz, boolean nullable, I defaultValue, String unit, I[] possibleValues, Class<O> outputClass) {
+        this(key, clazz, nullable, defaultValue, unit, possibleValues, outputClass, ValueOfConfigExtractor.getInstance());
+    }
+
+    public ExtractableProperty(String key, Class<I> clazz, boolean nullable, I defaultValue, String unit, I[] possibleValues, Class<O> outputClass, ConfigExtractor<I, O> extractor) {
+        Argument.notNull("key", key);
+        Argument.notNull("clazz", clazz);
+        Argument.notNull("outputClass", outputClass);
+        Argument.notNull("extractor", extractor);
+
+        this.key = key;
+        this.clazz = clazz;
+        this.nullable = nullable;
+        this.defaultValue = defaultValue;
+        this.unit = unit;
+        this.possibleValues = possibleValues;
+        this.outputClass = outputClass;
+        this.extractor = extractor;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Class<I> getClazz() {
+        return clazz;
+    }
+
+    public boolean isNullable() {
+        return nullable;
+    }
+
+    public I getDefaultValue() {
+        return defaultValue;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public I[] getPossibleValues() {
+        return possibleValues;
+    }
+
+    public Class<O> getOutputClass() {
+        return outputClass;
+    }
+
+    public ConfigExtractor<I, O> getExtractor() {
+        return extractor;
+    }
+
+    public O get() {
+        return Config.getProperty(key, clazz, nullable, defaultValue, unit, possibleValues, outputClass, extractor);
+    }
+
+    @Override
+    public int write(byte[] buffer, int index, Map<String, Object> context) {
+        index = BufferWriter.writeVersion(buffer, index, (byte) 1);
+        index = BufferWriter.writeString(buffer, index, key);
+        index = BufferWriter.writeString(buffer, index, clazz.getCanonicalName());
+        index = BufferWriter.writeBoolean(buffer, index, nullable);
+        index = BufferWriter.writeObjectRef(buffer, index, context, defaultValue);
+        index = BufferWriter.writeStringRef(buffer, index, unit);
+        index = BufferWriter.writeObjectArrayRef(buffer, index, context, possibleValues);
+        index = BufferWriter.writeString(buffer, index, outputClass.getCanonicalName());
+        index = BufferWriter.writeObject(buffer, index, context, extractor);
+        return index;
+    }
+
+    static {
+        BufferObjectType.registerBufferableClass(UUID.fromString("3b813ea8-472c-4267-be56-2d93916cc0b1"), ExtractableProperty.class);
+    }
 }

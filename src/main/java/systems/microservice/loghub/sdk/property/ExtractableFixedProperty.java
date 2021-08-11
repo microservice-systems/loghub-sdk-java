07bf8e6e5373ed3238e660c50431d8da82d19ad3
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-08-11 19:20:35 +0300

    added ExtractableFixedProperty class

--- src/main/java/systems/microservice/loghub/sdk/property/ExtractableFixedProperty.java
+++ src/main/java/systems/microservice/loghub/sdk/property/ExtractableFixedProperty.java
@@ -1,8 +1,67 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.sdk.property;
+
+import systems.microservice.loghub.sdk.Property;
+import systems.microservice.loghub.sdk.config.ConfigExtractor;
+import systems.microservice.loghub.sdk.config.extractor.ConfigValueOfExtractor;
+import systems.microservice.loghub.sdk.util.Argument;
+
+import java.io.Serializable;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class ExtractableFixedProperty {
+public class ExtractableFixedProperty<I, O> implements Property<O>, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected final I value;
+    protected final Class<O> outputClass;
+    protected final ConfigExtractor<I, O> extractor;
+
+    public ExtractableFixedProperty(I value, Class<O> outputClass) {
+        this(value, outputClass, ConfigValueOfExtractor.getInstance());
+    }
+
+    public ExtractableFixedProperty(I value, Class<O> outputClass, ConfigExtractor<I, O> extractor) {
+        Argument.notNull("outputClass", outputClass);
+        Argument.notNull("extractor", extractor);
+
+        this.value = value;
+        this.outputClass = outputClass;
+        this.extractor = extractor;
+    }
+
+    public I getValue() {
+        return value;
+    }
+
+    public Class<O> getOutputClass() {
+        return outputClass;
+    }
+
+    public ConfigExtractor<I, O> getExtractor() {
+        return extractor;
+    }
+
+    @Override
+    public O get() {
+        return extractor.extract(value, outputClass);
+    }
 }

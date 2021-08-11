1c36605c4e655b59261e0d80966b055b1921102f
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-08-11 14:59:06 +0300

    added FixedProperty class (1)

--- src/main/java/systems/microservice/loghub/sdk/http/client/HttpDefaultClient.java
+++ src/main/java/systems/microservice/loghub/sdk/http/client/HttpDefaultClient.java
@@ -18,8 +18,8 @@
 package systems.microservice.loghub.sdk.http.client;
 
 import systems.microservice.loghub.sdk.Property;
-import systems.microservice.loghub.sdk.property.RangeProperty;
 import systems.microservice.loghub.sdk.http.HttpClient;
+import systems.microservice.loghub.sdk.property.RangeProperty;
 import systems.microservice.loghub.sdk.util.Argument;
 import systems.microservice.loghub.sdk.util.Range;
 

9d95d548983ab61216c4eedf7aed919efb7626a4
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-08-12 16:16:16 +0300

    optimized imports

--- src/main/java/systems/microservice/loghub/sdk/config/Config.java
+++ src/main/java/systems/microservice/loghub/sdk/config/Config.java
@@ -17,7 +17,6 @@
 
 package systems.microservice.loghub.sdk.config;
 
-import systems.microservice.loghub.sdk.config.extractor.ConfigValueOfExtractor;
 import systems.microservice.loghub.sdk.util.Argument;
 import systems.microservice.loghub.sdk.util.MapUtil;
 import systems.microservice.loghub.sdk.util.Range;

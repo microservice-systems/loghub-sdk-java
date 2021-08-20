032066c1962e901b658aa62b6b0b62f9e09e6949
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-08-21 02:27:52 +0300

    added slug method to StringUtil class

--- src/main/java/systems/microservice/loghub/sdk/util/StringUtil.java
+++ src/main/java/systems/microservice/loghub/sdk/util/StringUtil.java
@@ -20,17 +20,26 @@ package systems.microservice.loghub.sdk.util;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
 public final class StringUtil {
-    public static final String EMPTY_STRING = "";
-    public static final String[] EMPTY_ARRAY = new String[0];
-    public static final String TEXT_PLAIN = "text/plain";
+    private static final HashSet<Character> slugChars = createSlugChars();
 
     private StringUtil() {
+    }
+
+    private static HashSet<Character> createSlugChars() {
+        HashSet<Character> cs = new HashSet<>(128);
+        cs.add('0'); cs.add('1'); cs.add('2'); cs.add('3'); cs.add('4'); cs.add('5'); cs.add('6'); cs.add('7'); cs.add('8'); cs.add('9');
+        cs.add('a'); cs.add('b'); cs.add('c'); cs.add('d'); cs.add('e'); cs.add('f'); cs.add('g'); cs.add('h'); cs.add('i'); cs.add('j');
+        cs.add('k'); cs.add('l'); cs.add('m'); cs.add('n'); cs.add('o'); cs.add('p'); cs.add('q'); cs.add('r'); cs.add('s'); cs.add('t');
+        cs.add('u'); cs.add('v'); cs.add('w'); cs.add('x'); cs.add('y'); cs.add('z');
+        cs.add('-');
+        return cs;
     }
 
     public static String cut(String value, int length) {
@@ -39,6 +48,41 @@ public final class StringUtil {
                 return value;
             } else {
                 return value.substring(0, length);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public static String slug(String value) {
+        if (value != null) {
+            value = cut(value, 63);
+            value = value.toLowerCase();
+            char[] cs = new char[value.length()];
+            for (int i = 0, ci = cs.length; i < ci; ++i) {
+                char c = value.charAt(i);
+                if (slugChars.contains(c)) {
+                    cs[i] = c;
+                } else {
+                    cs[i] = '-';
+                }
+            }
+            int b = 0;
+            for (int cb = cs.length; b < cb; ++b) {
+                if (cs[b] != '-') {
+                    break;
+                }
+            }
+            int e = cs.length - 1;
+            for (; e >= 0; --e) {
+                if (cs[e] != '-') {
+                    break;
+                }
+            }
+            if (b <= e) {
+                return new String(cs, b, e - b + 1);
+            } else {
+                return "";
             }
         } else {
             return null;

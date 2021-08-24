daa1c37a00cab5056692e9fd44396f4273f83d03
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-08-24 23:13:11 +0300

    added service, revision check methods to SpellUtil class & updated regex patterns in SpellUtil class

--- src/main/java/systems/microservice/loghub/sdk/util/SpellUtil.java
+++ src/main/java/systems/microservice/loghub/sdk/util/SpellUtil.java
@@ -26,20 +26,34 @@ import java.util.regex.Pattern;
  * @since 1.0
  */
 public final class SpellUtil {
-    private static final Pattern environmentPattern = Pattern.compile("^[a-z][a-z0-9-]{3,46}[a-z0-9]$");
-    private static final Pattern applicationPattern = Pattern.compile("^[a-z][a-z0-9-]{3,46}[a-z0-9]$");
-    private static final Pattern versionPattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,46}[a-zA-Z0-9]$");
-    private static final Pattern instancePattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,46}[a-zA-Z0-9]$");
-    private static final Pattern processPattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,46}[a-zA-Z0-9]$");
-    private static final Pattern userPattern = Pattern.compile("^[a-z][a-z0-9-]{3,46}[a-z0-9]$");
+    private static final Pattern servicePattern = Pattern.compile("^[a-z0-9-._]{1,253}$");
+    private static final Pattern environmentPattern = Pattern.compile("^[a-z][a-z0-9-]{1,61}[a-z0-9]$");
+    private static final Pattern applicationPattern = Pattern.compile("^[a-z][a-z0-9-]{1,61}[a-z0-9]$");
+    private static final Pattern versionPattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,125}[a-zA-Z0-9]$");
+    private static final Pattern revisionPattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,125}[a-zA-Z0-9]$");
+    private static final Pattern instancePattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,125}[a-zA-Z0-9]$");
+    private static final Pattern processPattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-._]{1,125}[a-zA-Z0-9]$");
+    private static final Pattern secretPattern = Pattern.compile("^[a-zA-Z0-9]{40}$");
+    private static final Pattern userPattern = Pattern.compile("^[a-z][a-z0-9-]{1,61}[a-z0-9]$");
     private static final Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$");
 
     private SpellUtil() {
     }
 
+    public static boolean isService(String service) {
+        if (service != null) {
+            int l = service.length();
+            if ((l >= 1) && (l <= 253)) {
+                return servicePattern.matcher(service).matches();
+            }
+        }
+        return false;
+    }
+
     public static boolean isEnvironment(String environment) {
         if (environment != null) {
-            if (environment.length() <= 48) {
+            int l = environment.length();
+            if ((l >= 3) && (l <= 63)) {
                 return environmentPattern.matcher(environment).matches();
             }
         }
@@ -48,7 +62,8 @@ public final class SpellUtil {
 
     public static boolean isApplication(String application) {
         if (application != null) {
-            if (application.length() <= 48) {
+            int l = application.length();
+            if ((l >= 3) && (l <= 63)) {
                 return applicationPattern.matcher(application).matches();
             }
         }
@@ -57,8 +72,19 @@ public final class SpellUtil {
 
     public static boolean isVersion(String version) {
         if (version != null) {
-            if (version.length() <= 48) {
+            int l = version.length();
+            if ((l >= 3) && (l <= 127)) {
                 return versionPattern.matcher(version).matches();
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRevision(String revision) {
+        if (revision != null) {
+            int l = revision.length();
+            if ((l >= 3) && (l <= 127)) {
+                return revisionPattern.matcher(revision).matches();
             }
         }
         return false;
@@ -66,7 +92,8 @@ public final class SpellUtil {
 
     public static boolean isInstance(String instance) {
         if (instance != null) {
-            if (instance.length() <= 48) {
+            int l = instance.length();
+            if ((l >= 3) && (l <= 127)) {
                 return instancePattern.matcher(instance).matches();
             }
         }
@@ -75,7 +102,8 @@ public final class SpellUtil {
 
     public static boolean isProcess(String process) {
         if (process != null) {
-            if (process.length() <= 48) {
+            int l = process.length();
+            if ((l >= 3) && (l <= 127)) {
                 return processPattern.matcher(process).matches();
             }
         }
@@ -84,8 +112,9 @@ public final class SpellUtil {
 
     public static boolean isSecret(String secret) {
         if (secret != null) {
-            if (secret.length() <= 128) {
-                return true;
+            int l = secret.length();
+            if (l == 40) {
+                return secretPattern.matcher(secret).matches();
             }
         }
         return false;
@@ -93,7 +122,8 @@ public final class SpellUtil {
 
     public static boolean isUser(String user) {
         if (user != null) {
-            if (user.length() <= 48) {
+            int l = user.length();
+            if ((l >= 3) && (l <= 63)) {
                 return userPattern.matcher(user).matches();
             }
         }
@@ -102,7 +132,8 @@ public final class SpellUtil {
 
     public static boolean isUrl(String url) {
         if (url != null) {
-            if (url.length() <= 4096) {
+            int l = url.length();
+            if ((l >= 1) && (l <= 4096)) {
                 try {
                     URL u = new URL(url);
                     return true;
@@ -115,7 +146,8 @@ public final class SpellUtil {
 
     public static boolean isEmail(String email) {
         if (email != null) {
-            if (email.length() <= 128) {
+            int l = email.length();
+            if ((l >= 3) && (l <= 127)) {
                 return emailPattern.matcher(email).matches();
             }
         }

27087d6a89137f40ac2e6f23a904a42de957878b
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-09-14 02:52:27 +0300

    added StreamException class

--- src/main/java/systems/microservice/loghub/sdk/util/StreamException.java
+++ src/main/java/systems/microservice/loghub/sdk/util/StreamException.java
@@ -1,8 +1,51 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.sdk.util;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class StreamException {
+public class StreamException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    protected final StreamOperation operation;
+    protected final long position;
+
+    public StreamException(StreamOperation operation, long position) {
+        super(String.format("[%s]: at position %d", Argument.notNull("operation", operation), Argument.inRangeLong("position", position, 0L, Long.MAX_VALUE)));
+
+        this.operation = operation;
+        this.position = position;
+    }
+
+    public StreamException(StreamOperation operation, long position, Throwable cause) {
+        super(String.format("[%s]: at position %d", Argument.notNull("operation", operation), Argument.inRangeLong("position", position, 0L, Long.MAX_VALUE)), cause);
+
+        this.operation = operation;
+        this.position = position;
+    }
+
+    public StreamOperation getOperation() {
+        return operation;
+    }
+
+    public long getPosition() {
+        return position;
+    }
 }

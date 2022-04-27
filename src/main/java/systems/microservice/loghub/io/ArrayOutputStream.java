f445a57b73289e0136f4a13ed20856592a20844b
Dmitry Kotlyarov <dmktv@microservice.systems>
2022-04-28 00:49:23 +0300

    added ArrayOutputStream class implementation

--- src/main/java/systems/microservice/loghub/io/ArrayOutputStream.java
+++ src/main/java/systems/microservice/loghub/io/ArrayOutputStream.java
@@ -17,9 +17,62 @@
 
 package systems.microservice.loghub.io;
 
+import systems.microservice.loghub.config.Validator;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class ArrayOutputStream {
+public final class ArrayOutputStream extends OutputStream {
+    private final int capacityMin;
+    private final int capacityMax;
+    private byte[] array;
+    private int index;
+
+    public ArrayOutputStream(int capacityMin, int capacityMax) {
+        Validator.inRangeInt("capacityMin", capacityMin, 0, Integer.MAX_VALUE);
+        Validator.inRangeInt("capacityMax", capacityMax, 0, Integer.MAX_VALUE);
+        if (capacityMin > capacityMax) {
+            throw new IllegalArgumentException(String.format("Argument 'capacityMin = %d' is greater than argument 'capacityMax = %d'", capacityMin, capacityMax));
+        }
+
+        this.capacityMin = capacityMin;
+        this.capacityMax = capacityMax;
+        this.array = new byte[capacityMin];
+        this.index = 0;
+    }
+
+    public int getCapacityMin() {
+        return capacityMin;
+    }
+
+    public int getCapacityMax() {
+        return capacityMax;
+    }
+
+    public byte[] getArray() {
+        return array;
+    }
+
+    public int getSize() {
+        return index;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        if (index >= array.length) {
+            byte[] a = new byte[(array.length > 0) ? array.length * 2 : 1];
+            System.arraycopy(array, 0, a, 0, array.length);
+            array = a;
+        }
+        array[index++] = (byte) b;
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+    }
 }

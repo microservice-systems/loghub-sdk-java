e4d887ced0cd5530332d17a3a547d6ec2e28bfe9
Dmitry Kotlyarov <dmktv@microservice.systems>
2022-05-01 23:32:53 +0300

    added SecretUtil class

--- src/main/java/systems/microservice/loghub/util/SecretUtil.java
+++ src/main/java/systems/microservice/loghub/util/SecretUtil.java
@@ -1,8 +1,40 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.util;
+
+import java.security.SecureRandom;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class SecretUtil {
+public final class SecretUtil {
+    private static final char[] SYMBOLS = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    private SecretUtil() {
+    }
+
+    public static String randomSecret() {
+        SecureRandom sr = new SecureRandom();
+        char[] s = new char[40];
+        for (int i = 0; i < 40; ++i) {
+            s[i] = SYMBOLS[sr.nextInt(SYMBOLS.length)];
+        }
+        return new String(s);
+    }
 }

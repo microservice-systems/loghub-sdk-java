0571d6b487859acfded2d32f60515704da534aa3
Dmitry Kotlyarov <dmktv@microservice.systems>
2022-05-01 23:31:57 +0300

    added RegexUtil class

--- src/main/java/systems/microservice/loghub/util/RegexUtil.java
+++ src/main/java/systems/microservice/loghub/util/RegexUtil.java
@@ -1,8 +1,77 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.util;
+
+import systems.microservice.loghub.config.Validator;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class RegexUtil {
+public final class RegexUtil {
+    private RegexUtil() {
+    }
+
+    public static Pattern compile(String regex) {
+        if (regex != null) {
+            return Pattern.compile(regex);
+        } else {
+            return null;
+        }
+    }
+
+    public static boolean match(Pattern regex, String value) {
+        if (regex != null) {
+            if (value != null) {
+                return regex.matcher(value).matches();
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public static List<String> match(Pattern regex, String[] values) {
+        Validator.notNull("values", values);
+
+        LinkedList<String> l = new LinkedList<>();
+        for (String v : values) {
+            if (match(regex, v)) {
+                l.add(v);
+            }
+        }
+        return l;
+    }
+
+    public static List<String> match(Pattern regex, List<String> values) {
+        Validator.notNull("values", values);
+
+        LinkedList<String> l = new LinkedList<>();
+        for (String v : values) {
+            if (match(regex, v)) {
+                l.add(v);
+            }
+        }
+        return l;
+    }
 }

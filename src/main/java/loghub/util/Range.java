b65b35e1a88d076d5bbdb278293c2a48741000b0
Dmitry Kotlyarov <dmktv@microservice.systems>
2022-05-04 03:01:59 +0300

    added Range class

--- src/main/java/loghub/util/Range.java
+++ src/main/java/loghub/util/Range.java
@@ -1,8 +1,59 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package loghub.util;
+
+import loghub.config.Validator;
+
+import java.io.Serializable;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class Range {
+public final class Range<T extends Comparable<T>> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public final T min;
+    public final T max;
+
+    public Range(T min, T max) {
+        Validator.notNull("min", min);
+        Validator.notNull("max", max);
+
+        this.min = min;
+        this.max = max;
+    }
+
+    public boolean isLeft(T value) {
+        Validator.notNull("value", value);
+
+        return value.compareTo(min) < 0;
+    }
+
+    public boolean isIn(T value) {
+        Validator.notNull("value", value);
+
+        return (value.compareTo(min) >= 0) && (value.compareTo(max) <= 0);
+    }
+
+    public boolean isRight(T value) {
+        Validator.notNull("value", value);
+
+        return value.compareTo(max) > 0;
+    }
 }
